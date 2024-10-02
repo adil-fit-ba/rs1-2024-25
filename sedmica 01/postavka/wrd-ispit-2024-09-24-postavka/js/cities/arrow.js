5ed99981be1738c4396ba297090ced9804331b1d
Adil Joldic <adil@fit.ba>
2024-10-02 08:32:33 +0200

    rename

--- sedmica 01/postavka/wrd-ispit-2024-09-24-postavka/js/cities/arrow.js
+++
@@ -1,5 +0,0 @@
-generateArrow = (id) => {
-    return `<svg id= ${"wind-indicator-" + id} xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100">
-    <polygon points="50,10 60,30 50,25 40,30" fill="black" />
-    <rect x="48" y="30" width="4" height="50" fill="black" />
-  </svg>`}
