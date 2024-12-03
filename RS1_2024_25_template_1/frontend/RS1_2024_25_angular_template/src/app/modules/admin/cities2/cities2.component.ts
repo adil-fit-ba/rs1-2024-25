45015566c58796a5e756f2209f171cf95be971be
Adil Joldic <adil@fit.ba>
2024-12-03 20:28:47 +0100

    19-admin-cities-overview-dialog-za-brisanje
    dodat dialog sa potvrdom

--- RS1_2024_25_template_1/frontend/RS1_2024_25_angular_template/src/app/modules/admin/cities2/cities2.component.ts
+++ RS1_2024_25_template_1/frontend/RS1_2024_25_angular_template/src/app/modules/admin/cities2/cities2.component.ts
@@ -5,6 +5,8 @@ import {
   CityGetAll1Response
 } from '../../../endpoints/city-endpoints/city-get-all1-endpoint.service';
 import {CityDeleteEndpointService} from '../../../endpoints/city-endpoints/city-delete-endpoint.service';
+import {MyDialogConfirmComponent} from '../../shared/dialogs/my-dialog-confirm/my-dialog-confirm.component';
+import {MatDialog} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-cities2',
@@ -20,7 +22,8 @@ export class Cities2Component {
   constructor(
     private cityGetService: CityGetAll1EndpointService,
     private cityDeleteService: CityDeleteEndpointService,
-    private router: Router
+    private router: Router,
+    private dialog: MatDialog
   ) {
   }
 
@@ -40,14 +43,33 @@ export class Cities2Component {
   }
 
   deleteCity(id: number): void {
-    if (confirm('Are you sure you want to delete this city?')) {
-      this.cityDeleteService.handleAsync(id).subscribe({
-        next: () => {
-          console.log(`City with ID ${id} deleted successfully`);
-          this.cities = this.cities.filter(city => city.id !== id); // Uklanjanje iz lokalne liste
-        },
-        error: (err) => console.error('Error deleting city:', err)
-      });
-    }
+
+    this.cityDeleteService.handleAsync(id).subscribe({
+      next: () => {
+        console.log(`City with ID ${id} deleted successfully`);
+        this.cities = this.cities.filter(city => city.id !== id); // Uklanjanje iz lokalne liste
+      },
+      error: (err) => console.error('Error deleting city:', err)
+    });
+  }
+
+  openMyConfirmDialog(id: number) {
+    const dialogRef = this.dialog.open(MyDialogConfirmComponent, {
+      width: '350px',
+      data: {
+        title: 'Potvrda brisanja',
+        message: 'Da li ste sigurni da želite obrisati ovu stavku?'
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        console.log('Korisnik je potvrdio brisanje');
+        // Pozovite servis ili izvršite logiku za brisanje
+        this.deleteCity(id);
+      } else {
+        console.log('Korisnik je otkazao brisanje');
+      }
+    });
   }
 }
